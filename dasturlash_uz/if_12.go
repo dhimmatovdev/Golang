b53c6ca8f44587e2f9cffe09cd12cf0caf41d8a4
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-08 23:06:30 +0500

    oson

---
+++ dasturlash_uz/if_12.go
@@ -0,0 +1,16 @@
+/*
+Misol Sharti
+a, b, c sonlari berilgan. Berilgan uchta sonning o'rtacha qiymatini  hisoblovchi dastur yozing  va shu sonni konsolga chiqaring. 
+*/
+package main 
+import ("fmt") 
+
+func main() {
+	var (
+		a = 5 
+		b = 6 
+		c = 9
+	)
+	var avr=(a+b+c)/3
+	fmt.Println(avr)
+}
