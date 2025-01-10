379bf9a32adb9a2cbb8eaf5981f33b091ab55481
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-10 23:52:58 +0500

    for

---
+++ dasturlash_uz/for_loop/for_1.go
@@ -0,0 +1,18 @@
+/*
+Misol Sharti
+a soni berilgan.  0 dan a gacha bo'lgan sonlaryig'indisini 
+toping  va yig'indini konsolga chiqaring. a sonini ham hisobga oling.
+*/
+
+package main
+import ("fmt")
+
+func main() {
+	var a int
+	fmt.Scan(&a)
+	var sum int 
+	for i:=0; i<=a; i++ {
+		sum +=i
+	}
+	fmt.Print(sum)
+}
