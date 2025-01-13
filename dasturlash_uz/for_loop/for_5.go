5d185246365ad03351a704942a5b9362281ba520
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-13 23:55:21 +0500

    fordan

---
+++ dasturlash_uz/for_loop/for_5.go
@@ -0,0 +1,14 @@
+/*
+Misol Shartif
+a soni berilgan. 0 dan a gacha bo'lgan juft sonlar soni toping va konsolga chiqaring. a sonini inobatga olmaymiz.
+
+
+*/
+package main 
+import ("fmt") 
+
+func main() {
+	var a int 
+	sum:=0
+	
+}
